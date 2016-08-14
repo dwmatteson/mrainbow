a2d34be8294bfdf2f14b7b81565e9cafa56cb86e
David Matteson <dwmatteson@gmail.com>
2016-08-14 08:29:31 -0400

    Remove dev password from mr-websocket.js
    Put in some placeholder text instead to make it clear users should pick their own password.

--- mr-websocket.js
+++ mr-websocket.js
@@ -5,7 +5,7 @@ var	MRAPI = require('./mr-api.js'),
 	mr_api = MRAPI.create({
 	        'mysql_server' : 'localhost',
 	        'mysql_username' : 'mrainbow',
-	        'mysql_password' : 'pVal!mdx8Q-D#',
+	        'mysql_password' : 'REPLACE ME WITH A REAL PASSWORD',
 	        'mysql_database' : 'mrainbow'
 	}),
 	WebwsServer = require('ws').Server,
